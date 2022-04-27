5f2bc3bdfadea0d3086d55faf283c45136f99d08
KIMSEUNGGYU <dev.seunggyu@gmail.com>
2022-04-27 10:25:46 +0900

    [refactor] 불필요한 코드 제거 및 app 컴포넌트 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from '@emotion/styled';
+
+// import Toggle from './components/toggle';
+import Tab from './components/Tab';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <h1>TEST</h1>
+      {/* <ComponentWrapper>
+        <h3>1. toggle</h3>
+        <Toggle />
+      </ComponentWrapper> */}
+      <ComponentWrapper>
+        <h3>2. toggle</h3>
+        <Tab data={['감자', '고구마', '카레라이스']} width={'70%'} />
+      </ComponentWrapper>
+    </Wrapper>
   );
 }
+
+const Wrapper = styled.div(() => ({
+  padding: '2rem',
+}));
+
+const ComponentWrapper = styled.div(() => ({
+  marign: '0 auto',
+  height: '8rem',
+  borderBottom: '1px solid #ebebeb',
+}));
 
 export default App;
